bfb9d8ab8ed67e197189ace5c002f8139de5cc55
binjian xin <binjian.xin@newrizon.com>
2024-04-10 09:50:05 +0800

    [change] bump to python 3.11

--- candycan/dbc.py
+++ candycan/dbc.py
@@ -6,5 +6,6 @@ __all__ = []
 # %% ../nbs/06.dbc.ipynb 4
 import cantools
 from cantools.database import Message as MessageTpl
+
 # from can.interface import Bus
-# from can import Message 
+# from can import Message
